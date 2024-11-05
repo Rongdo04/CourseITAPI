385ec29f3a9b16726417a1610b6fdeeeb62717d8
Rongdo04 <53899673+Rongdo04@users.noreply.github.com>
2024-11-05 20:53:16 +0700

    Update Program.cs

--- QLHL/Program.cs
+++ QLHL/Program.cs
@@ -53,11 +53,10 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
